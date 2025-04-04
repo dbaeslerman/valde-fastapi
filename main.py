fb97672c1ba5b04e84e0a16da5ceb86dfd03cd36
dbaeslerman <halloween.dri@gmail.com>
2025-04-04 20:14:38 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from pymongo import MongoClient
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+app = FastAPI()
+
+# Conexión a MongoDB
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["valde_db"]
+collection = db["reportes"]
+
+@app.get("/")
+def home():
+    return {"msg": "Valde API funcionando 👌"}
+
+@app.post("/reporte")
+async def nuevo_reporte(request: Request):
+    data = await request.json()
+    collection.insert_one(data)
+    return {"msg": "Reporte guardado", "data": data}
